7ca614ee9516c8417779e8364f0d342ac1bd0793
Andrew Sander <andrsand1@gmail.com>
2023-10-04 12:12:27 -0600

    i think i finally solved env variable problem

--- server/build-config.js
+++ server/build-config.js
@@ -4,11 +4,11 @@ const fs = require('fs');
 const config = require('../src/amplifyconfiguration.json');
 
 // Inject environment variables
-config.aws_project_region = process.env.AWS_PROJECT_REGION;
-config.aws_cognito_identity_pool_id = process.env.AWS_COGNITO_IDENTITY_POOL_ID;
-config.aws_cognito_region = process.env.AWS_COGNITO_REGION;
-config.aws_user_pools_id = process.env.AWS_USER_POOLS_ID;
-config.aws_user_pools_web_client_id = process.env.AWS_USER_POOLS_WEB_CLIENT_ID;
+config.aws_project_region = process.env.REACT_APP_AWS_PROJECT_REGION;
+config.aws_cognito_identity_pool_id = process.env.REACT_APP_AWS_COGNITO_IDENTITY_POOL_ID;
+config.aws_cognito_region = process.env.REACT_APP_AWS_COGNITO_REGION;
+config.aws_user_pools_id = process.env.REACT_APP_AWS_USER_POOLS_ID;
+config.aws_user_pools_web_client_id = process.env.REACT_APP_AWS_USER_POOLS_WEB_CLIENT_ID;
 // Add more variables as needed
 
 // Write the modified configuration back to configuration.json
--- src/Components/Blogs.js
+++ src/Components/Blogs.js
@@ -6,7 +6,7 @@ import CreatePost from "./CreatePost";
 import { Modal } from "react-bootstrap";
 
 export default function Blogs({ user }) {
-    const apiUrl = user ? `https://lords-of-blogtown.onrender.com/api/blogs/me/${user.username}` : `https://lords-of-blogtown.onrender.com/api/blogs/`;
+    const apiUrl = user ? `https://lords-of-blogtown.onrender.com:10000/api/blogs/me/${user.username}` : `https://lords-of-blogtown.onrender.com:10000/api/blogs/`;
 
     const { data: blogs, isPending, error } = useFetch(apiUrl);
     const [newBlogs, setNewBlogs] = useState();
